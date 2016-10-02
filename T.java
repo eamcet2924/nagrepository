935a33bb7e7af823e3317334081eac9e217735da
eamcet2924 <nagarjuna0409@gmail.com>
2016-10-02 12:21:08 -0700

    Add files via upload
    hi

---
+++ T.java
@@ -0,0 +1,16 @@
+package com.samples;
+import org.apache.commons.lang3.StringUtils;
+
+
+public class T {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = "AASSSSSS123";
+		s = StringUtils.replace(s, "123", "456");
+		System.out.println(s);
+		
+
+	}
+
+}
